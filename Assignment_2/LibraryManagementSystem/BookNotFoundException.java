97ba70dd0bcdfbc7ead3362c0ef7f848df853e86
Dhanish Chander <111438379+Dhanish27@users.noreply.github.com>
2025-02-10 17:44:35 +0530

    Small Change

---
+++ Assignment_2/LibraryManagementSystem/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package Assignment_2.LibraryManagementSystem;
+
+public class BookNotFoundException extends Exception {
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+}
